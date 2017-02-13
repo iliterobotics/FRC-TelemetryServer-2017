93c9444f0f47c95fe7ca43b64cc7dc34a5720e0c
Michael Kelly <rockymkelly@gmail.com>
2017-02-13 17:37:30 -0500

    Added socketio communication for updates

--- routes/constants.js
+++ routes/constants.js
@@ -1,6 +1,18 @@
 var ConstantValue = require('../models/constant_value');
 
 module.exports = function(app){
+  var http = require('http').Server(app);
+  var io = require('socket.io')(http);
+
+  http.listen(1181, function(){
+    console.log('Constant update stream socket enabled');
+  });
+
+  app.get('/constants', function(req, res, err){
+    ConstantValue.find({}, function(err, docs){
+      res.send(docs);
+    });
+  });
   app.get('/constant/:name', function(req, res, err){
     ConstantValue.findOne({Name: req.params.name}, function(err, constant){
       if(err){
@@ -17,14 +29,15 @@ module.exports = function(app){
   });
   app.post('/constant/set', function(req, res, err){
     var constant = req.body;
-    ConstantValue.find({Name : constant.Name}, function(err, constant){
-      if(!constant){
+    ConstantValue.findOne({Name : constant.Name}, function(err, found){
+      if(!found){
         res.send('CONSTANT DOES NOT EXIST');
       }
       else {
         ConstantValue.update({Name: constant.Name}, {$set:{Value: constant.Value}},function(err, doc){
           if(err) return console.error(err);
           res.send(doc);
+          io.emit('constants-updated', {});
         });
       }
     });
@@ -32,20 +45,30 @@ module.exports = function(app){
   app.post('/constant/add', function(req, res, err){
     var name = req.body.Name;
     var value = req.body.Value;
+    var description = req.body.Description;
     var constant = new ConstantValue({
       Name: name,
-      Value: value
+      Value: value,
+      Description: description
     })
     ConstantValue.findOne({Name:constant.Name}, function(err, doc){
       if(!doc){
         constant.save(function(err){
             console.log('New constant ' + constant.Name + ' added with value: ' + constant.Value);
             res.send('Value added');
+            io.emit('constants-updated', {});
         });
       }
       else{
         res.send('Value already exists');
       }
+    });
+  });
+  app.get('/constant/remove/:name', function(req, res, err){
+    console.log('Attempting to remove constant');
+    ConstantValue.remove({Name: req.params.name}, function(err){
+      res.send('done');
+      io.emit('constants-updated', {});
     });
   });
 }
